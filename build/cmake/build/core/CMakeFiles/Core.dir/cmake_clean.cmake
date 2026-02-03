c6dd27bb52897c3e5869e9cbdabcffad37b2129d
sakura2107 <caijinpeisg@163.com>
2026-02-03 17:04:09 +0800

    Add CmakeList

---
+++ build/cmake/build/core/CMakeFiles/Core.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/devboost/xeMemCmp.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/devboost/xeMemCmp.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/devboost/xeMemFind.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/devboost/xeMemFind.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/filesystem/xeFileMmapStream.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/filesystem/xeFileMmapStream.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/filesystem/xePath.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/filesystem/xePath.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/log/xeLogOutpout.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/log/xeLogOutpout.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/memory/xeAlloc.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/memory/xeAlloc.cpp.o.d"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/string/xeString.cpp.o"
+  "CMakeFiles/Core.dir/home/sara/Downloads/Xe/src/core/string/xeString.cpp.o.d"
+  "libCore.a"
+  "libCore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
